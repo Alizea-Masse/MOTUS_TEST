3a177ac9592801d2d4c76eba3bf3ffaa120937d7
unknown <alizea.masse@gmail.com>
2023-06-03 09:12:51 +0200

    submit data function

--- LoginRegisterAjax/script.php
+++ LoginRegisterAjax/script.php
@@ -1,1 +1,25 @@
-<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+  function submitData(){
+    $(document).ready(function(){
+      let data = {
+        name: $("#name").val(),
+        username: $("#username").val(),
+        password: $("#password").val(),
+        action: $("#action").val(),
+      };
+
+      $.ajax({
+        url: 'function.php',
+        type: 'post',
+        data: data,
+        success:function(response){
+          alert(response);
+          if(response == "Login Successful"){
+            window.location.reload();
+          }
+        }
+      });
+    });
+  }
+</script>
