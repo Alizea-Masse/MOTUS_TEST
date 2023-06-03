e868114767f9137eb72d9c13b206be3e87dfa149
unknown <alizea.masse@gmail.com>
2023-06-03 09:26:26 +0200

    register function

--- LoginRegisterAjax/function.php
+++ LoginRegisterAjax/function.php
@@ -1,6 +1,39 @@
 <?php
 session_start();
-$conn = new PDO ('mysql:host=localhost;dbname=motus;charset=utf8;','root','');
+$conn = mysqli_connect("localhost", "root", "", "motus");
 
+// IF
+if(isset($_POST["action"])){
+    if($_POST["action"] == "register"){
+      register();
+    }
+    else if($_POST["action"] == "login"){
+      login();
+    }
+  }
+
+  // REGISTER
+function register(){
+    global $conn;
+  
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+  
+    if(empty($name) || empty($username) || empty($password)){
+      echo "Please Fill Out The Form!";
+      exit;
+    }
+  
+    $user = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username'");
+    if(mysqli_num_rows($user) > 0){
+      echo "Username Has Already Taken";
+      exit;
+    }
+  
+    $query = "INSERT INTO tb_user VALUES('', '$name', '$username', '$password')";
+    mysqli_query($conn, $query);
+    echo "Registration Successful";
+  }
 
 ?>
