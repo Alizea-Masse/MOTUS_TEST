69af6f57ae3c22a28089bc8ed40afd46f58c4895
unknown <alizea.masse@gmail.com>
2023-06-03 09:29:27 +0200

    add login function

--- LoginRegisterAjax/function.php
+++ LoginRegisterAjax/function.php
@@ -36,4 +36,33 @@ function register(){
     echo "Registration Successful";
   }
 
+  // LOGIN
+function login(){
+    global $conn;
+  
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+  
+    $user = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username'");
+  
+    if(mysqli_num_rows($user) > 0){
+  
+      $row = mysqli_fetch_assoc($user);
+  
+      if($password == $row['password']){
+        echo "Login Successful";
+        $_SESSION["login"] = true;
+        $_SESSION["id"] = $row["id"];
+      }
+      else{
+        echo "Wrong Password";
+        exit;
+      }
+    }
+    else{
+      echo "User Not Registered";
+      exit;
+    }
+  }
+
 ?>
